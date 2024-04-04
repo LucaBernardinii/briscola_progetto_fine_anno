9315cce2702d5dbf69f59743c20884c853fa0f83
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2024-04-04 13:56:58 +0200

    implementazione funzioni

--- briscola.py
+++ briscola.py
@@ -13,3 +13,35 @@ def crea_mazzo(semi: list, valori: list) -> list[dict]:
         for valore in valori:
             mazzo.append({"seme": seme, "valore": valore})
     return mazzo
+
+#funzione per estrarre la carta
+def estrai_carta (mazzo: list [dict]) -> dict:
+    carta = random.choice (mazzo)
+    mazzo.remove (carta)
+    return carta
+
+#funzione per scelta della briscola
+def scelta_briscola(mazzo: list [dict]) -> dict:
+    carta_briscola = random.choice (mazzo)
+    mazzo.remove (carta_briscola)
+    mazzo.append (carta_briscola)
+
+
+
+
+
+
+#svolgimento partita
+print("Italiano->I")
+print("Romagnolo->R")
+scelta_lingua = input("Scegli la lingua con cui giocare: ")
+if scelta_lingua == "I":
+    print("La partita si svolgerà in italiano.")
+elif scelta_lingua == "R":
+    print("Us zuga in rumagnol.")
+else:
+    print("Scelta non valida, invurnid.")
+
+
+
+mazzo = crea_mazzo (["denari","coppe","bastoni","spade"],["Asso",2,3,4,5,6,7,"Fante","Cavallo","Re"])
