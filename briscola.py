1f0176c3a2472b83271159004296bb97a4e60d8c
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2024-04-16 17:07:48 +0200

    implementazione e rifinitura di varie funzioni

--- briscola.py
+++ briscola.py
@@ -11,7 +11,7 @@ def crea_mazzo(semi: list, valori: list) -> list[dict]:
     mazzo = []
     for seme in semi:
         for valore in valori:
-            mazzo.append({"seme": seme, "valore": valore})
+            mazzo.append({"seme": seme, "valore": valore, "briscola": False})
     return mazzo
 
 #funzione per estrarre la carta
@@ -31,8 +31,56 @@ def scelta_briscola(mazzo: list [dict]) -> dict:
         seme = carta["seme"]
         if seme_briscola == seme:
             lista_briscole.append(carta)
+    for carta in lista_briscole:
+        carta["briscola"] = True
+    return carta_briscola
 
+#funzione per assegnazione dei valori delle carte
+def assegna_valori(carta: dict) -> int:
+    if carta["valore"] == "Asso" or carta["valore"] == "Ass":
+        return 11
+    elif carta["valore"] == "Fante" or carta["valore"] == "Fent":
+        return 2
+    elif carta["valore"] == "Cavallo" or carta["valore"] == "Caval":
+        return 3
+    elif carta["valore"] == "Re":
+        return 4
+    elif carta["valore"] == 3:
+        return 10
+    else:
+        return 0
+    
+#funzione per la distribuzione iniziale delle carte
+def pesca_carte(mazzo: list[dict]) -> list[dict]:
+    mano_giocatore = []
+    mano_computer = []
+    for i in range(3):
+        carta_estratta = random.choice(mazzo)
+        mano_giocatore.append(carta_estratta)
+        mazzo.remove(carta_estratta)
+    for x in range(3):
+        carta_estratta = random.choice(mazzo)
+        mano_computer.append(carta_estratta)
+        mazzo.remove(carta_estratta)
+    return mano_giocatore, mano_computer            #check
 
+#funzione per la partita in italiano
+def partita_italiano(mazzo: list [dict]) -> int:
+    punteggio_giocatore = 0
+    punteggio_computer = 0
+    mazzo = crea_mazzo (["denari","coppe","bastoni","spade"],["Asso",2,3,4,5,6,7,"Fante","Cavallo","Re"])
+    mischia_mazzo(mazzo)
+    while len(mazzo) > 0:
+        
+
+#funzione per la partita in romagnolo
+def partita_romagnolo(mazzo: list [dict]) -> int:
+    punteggio_giocatore = 0
+    punteggio_computer = 0
+    mazzo = crea_mazzo (["danèri","copp","bastun","spede"],["Ass",2,3,4,5,6,7,"Fent","Caval","Re"])
+    mischia_mazzo(mazzo)
+    while len(mazzo) > 0:
+    
 
 
 
@@ -48,11 +96,14 @@ print("Romagnolo->R")
 scelta_lingua = input("Scegli la lingua con cui giocare: ")
 if scelta_lingua == "I" or scelta_lingua == "i":
     print("La partita si svolgerà in italiano.")
+    nome_giocatore = input("Inserisci il tuo nome?: ")
+    
 elif scelta_lingua == "R" or scelta_lingua == "r":
     print("Us zuga in rumagnol.")
+    nome_giocatore = input("Cum ut cem?: ")
+    
 else:
     print("Scelta non valida, invurnid.")
 
 
 
-mazzo = crea_mazzo (["denari","coppe","bastoni","spade"],["Asso",2,3,4,5,6,7,"Fante","Cavallo","Re"])
